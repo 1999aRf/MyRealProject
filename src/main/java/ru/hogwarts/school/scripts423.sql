f5182be19b4fe71ac0c6f396f6a4d708eea78f5f
rinatagliulov <rinat.agliulov@rentready.com>
2024-06-02 07:45:44 -0400

    Домашняя работа по уроку "4.2. Управление схемами баз данных" исправлена и готова к проверке.

--- src/main/java/ru/hogwarts/school/scripts423.sql
+++ src/main/java/ru/hogwarts/school/scripts423.sql
@@ -6,3 +6,14 @@ FROM
     Student s
 JOIN
     Faculty f ON s.faculty_id = f.id;
+
+SELECT
+    s.name AS student_name,
+    s.age AS student_age,
+    f.name AS faculty_name
+FROM
+    Student s
+JOIN
+    Faculty f ON s.faculty_id = f.id
+WHERE
+    s.avatar IS NOT NULL;
