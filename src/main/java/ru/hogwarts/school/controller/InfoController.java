f53ddd0ecb5eb8ea6b8ea186392732112ffa387a
rinatagliulov <rinat.agliulov@rentready.com>
2024-06-15 11:10:36 -0400

    В домашнем задании для урока"4.4 Логирование и конфигурирование в приложении" было добавлен новый контроллер и эндпоинт для отображения порта в этом commit.

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,8 +1,16 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
+    @Value("${server.port}")
+    private int serverPort;
 
+    @GetMapping("/port")
+    public String getServerPort() {
+        return "Application is running on port: " + serverPort;
+    }
 }
