f32eeee52d8f1c6e9689540e9efe7443a91c0e25
rinatagliulov <rinat.agliulov@rentready.com>
2024-06-16 13:47:24 -0400

    В домашнем задании для урока"4.5 Параллельные стримы" был реализован 4 шаг. В нем добавлен 1 новый эндпоинт и реализация для info.

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,16 +1,26 @@
 package ru.hogwarts.school.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.hogwarts.school.service.InfoService;
 
 @RestController
+@RequestMapping("/info")
 public class InfoController {
-    @Value("${server.port}")
-    private int serverPort;
+    private final InfoService infoService;
+
+    public InfoController(InfoService infoService) {
+        this.infoService = infoService;
+    }
 
     @GetMapping("/port")
-    public String getServerPort() {
-        return "Application is running on port: " + serverPort;
+    public String getPort() {
+        return infoService.getPort();
+    }
+
+    @GetMapping("/calculate")
+    public void calculate() {
+            infoService.calculate();
     }
 }
